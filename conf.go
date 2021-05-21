14ed7e2917685989633ce4e92094063388ac80b3
jiaoge <yingjunjiao@outlook.com>
2021-05-21 10:03:15 +0800

    add nacos

--- conf.go
+++ conf.go
@@ -28,4 +28,27 @@ func Init(appName string) {
 	}
 
 	v.MergeInConfig()
+
+	// initRemote()
+}
+
+func initRemote() {
+	if v.GetString("remote.provider") == "nacos" {
+		port := v.GetUint64("remote.port")
+		if port == 0 {
+			port = 8080
+		}
+		// nacos.NewNacosConfigManager(&nacos.Option{
+		// 	Url:         v.GetString("remote.url"),
+		// 	Port:        port,
+		// 	NamespaceId: "public",
+		// 	GroupName:   "DEFAULT_GROUP",
+		// 	Config:      nacos.Config{DataId: "config_dev"},
+		// 	Auth:        nil,
+		// })
+
+		v.AddRemoteProvider("", "", "")
+
+	}
+
 }
